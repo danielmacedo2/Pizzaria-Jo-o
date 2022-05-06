9e22add9c2035abbd86276a52ca271e891623d7e
Daniel Macedo <danielmsv2011@hotmail.com>
2022-05-06 09:54:37 -0300

    Dashboard do site

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,5 @@
 <footer>
-        <p>Rodapé</p>
+        <p>Pizzaria do João &copy; 2022</p>
     </footer>
     <!-- JavaScript Bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
