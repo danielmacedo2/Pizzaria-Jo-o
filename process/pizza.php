1538b585edff6ea982de82772b76abcfafc831b5
Daniel Macedo <danielmsv2011@hotmail.com>
2022-05-09 10:44:02 -0300

    Dados resgatados

--- process/pizza.php
+++ process/pizza.php
@@ -1,3 +1,24 @@
 <?php
     include_once('conn.php');
+
+    $method = $_SERVER["REQUEST_METHOD"];
+
+    // Resgate dos dados, montagem do pedido
+    if($method === "GET") {
+
+        $bordasQuery = $conn->query("SELECT * FROM bordas;");
+        
+        $bordas = $bordasQuery->fetchAll();
+
+        $massasQuery = $conn->query("SELECT * FROM massas;");
+
+        $massas = $massasQuery->fetchAll();
+
+        $saboresQuery = $conn->query("SELECT * FROM sabores;");
+
+        $sabores = $saboresQuery->fetchAll();
+
+    } else if($method === "POST") {
+
+    }
 ?>
