6d27b989ee6ee045a41ca4cf82f1990bf2fc997e
Daniel Macedo <danielmsv2011@hotmail.com>
2022-05-12 12:54:48 -0300

    Atualização de status do pedido adicionado

--- process/orders.php
+++ process/orders.php
@@ -104,13 +104,26 @@
 
             $deleteQuery = $conn->prepare("DELETE FROM pedidos WHERE pizza_id = :pizza_id;");
 
-            $deleteQuery->bindParam('pizza_id', $pizzaId, PDO::PARAM_INT);
+            $deleteQuery->bindParam(':pizza_id', $pizzaId, PDO::PARAM_INT);
 
             $deleteQuery->execute();
 
             $_SESSION["msg"] = 'Pedido removido com sucesso!';
             $_SESSION["status"] = "success";
 
+        } else if ($type === 'update') {
+            $pizzaId = $_POST['id'];
+            $statusId = $_POST["status"];
+            
+            $updateQuery = $conn->prepare('UPDATE pedidos SET status_id = :status_id WHERE pizza_id = :pizza_id;');
+
+            $updateQuery->bindParam(':pizza_id', $pizzaId, PDO::PARAM_INT);
+            $updateQuery->bindParam(':status_id', $statusId, PDO::PARAM_INT);
+
+            $updateQuery->execute();
+
+            $_SESSION['msg'] = "Pedido atualizado com sucesso!";
+            $_SESSION['status'] = 'success';
         }
 
         // retorna usuário para dashboard
